33074730931d4ea88d79877780f365f1837d81c4
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-17 20:20:55 +0700

    update gitignore

--- resources/views/themes/sneat/partials/footer.blade.php
+++ resources/views/themes/sneat/partials/footer.blade.php
@@ -2,10 +2,7 @@
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
     <div class="mb-2 mb-md-0">
-        ©
-        {{date('Y')}}
-        made with ❤️ by
-        <a href="https://nostrasolution.com" target="_blank" class="footer-link fw-bolder">Nostra Solution</a>
+        © {{date('Y')}} made with ❤️ by <a href="https://nostrasolution.com" target="_blank" class="footer-link fw-bolder">Nostra Solution</a>
     </div>
     </div>
 </footer>
