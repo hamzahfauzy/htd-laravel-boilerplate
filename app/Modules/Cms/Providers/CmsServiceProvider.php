b539e5c547c859e84436c6c9fb87697c5e4d1091
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-05-26 18:08:56 +0700

    change provider sensitive case

--- app/Modules/Cms/Providers/CmsServiceProvider.php
+++ app/Modules/Cms/Providers/CmsServiceProvider.php
@@ -8,7 +8,7 @@ class CmsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../Databases/Migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../Databases/migrations');
         // $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
         // $this->loadViewsFrom(__DIR__.'/../Resources/views', 'cms');
     }
